b1b908615d60025404bf781e7cb1c20e64093100
Ssrdeshmukh <113228653+Ssrdeshmukh@users.noreply.github.com>
2024-01-24 19:22:26 +0530

    Add files via upload

---
+++ Ice-Cream/script.js
@@ -0,0 +1,39 @@
+setInterval(show1, 1000);
+        function show1() {
+            a = document.getElementById("j1");
+            if (a.style.display = "none") {
+                a.style.display = "block"
+            }
+        }
+
+        setInterval(show2, 2000);
+        function show2() {
+            a = document.getElementById("j2");
+            if (a.style.display = "none") {
+                a.style.display = "block"
+            }
+        }
+
+        setInterval(show3, 2500);
+        function show3() {
+            a = document.getElementById("j3");
+            if (a.style.display = "none") {
+                a.style.display = "block"
+            }
+        }
+
+        setInterval(show4, 3000);
+        function show4() {
+            a = document.getElementById("j4");
+            if (a.style.display = "none") {
+                a.style.display = "block"
+            }
+        }
+
+        // setInterval(show5, 3500);
+        // function show5() {
+        //     a = document.getElementById("j5");
+        //     if (a.style.display = "none") {
+        //         a.style.display = "block"
+        //     }
+        // }
